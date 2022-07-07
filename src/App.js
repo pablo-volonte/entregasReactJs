c8311504e67299c13210feced600c8c82092bd5d
Pablo Volonté <pablovolonte@MacBook-Pro-de-Pablo.local>
2022-07-07 20:40:27 -0300

    Entrega componentes I - Volonte

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "./components/navBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/navBar";
+import Carrito from "./components/carWidget";
 
 function App() {
 	return <NavBar />;
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,18 +1,17 @@
 function NavBar() {
 	return (
-		<div>
-			<header className=" navbar bg-primary">
-				<h2 className=" link-light navbar-brand ">
+		<>
+			<header className="navbar bg-primary">
+				<h2 className="link-light navbar-brand ">
 					Tienda Chanchito Feliz
 				</h2>
 				<ul className="nav">
 					<a className="link-light nav-link active">Home</a>
 					<a className="link-light nav-link active">Productos</a>
 					<a className="link-light nav-link active">Ofertas</a>
-					<a className="link-light nav-link active">Carrito</a>
 				</ul>
 			</header>
-		</div>
+		</>
 	);
 }
 
